d0d8ed0ae22ac85e6fd17b788a4fa1a95b641ee4
Imran Javed <96229355+ImranJaved1073@users.noreply.github.com>
2022-11-15 19:42:29 +0500

    Update Source.cpp

--- LABS/Lab 09 Inheritence/Task2/Source.cpp
+++ LABS/Lab 09 Inheritence/Task2/Source.cpp
@@ -8,53 +8,49 @@ using namespace std;
 
 int main()
 {
-	{
-		ApplePhone iphone;
-		iphone.setMfgID(1);
-		iphone.setMfgDate("11/3/2022");
-		iphone.setMfgName("Apple");
-		iphone.setiOSVersion("iOS 14");
-		iphone.turnOn();
-		iphone.launchSiri();
-		iphone.launchPaper();
-		iphone.makeVoiceCall(3057);
-		iphone.recordAudio();
-		iphone.turnOff();
-		iphone.sendSMS("Hello, World?", 305);
-		iphone.turnOn();
-		iphone.makeVoiceCall(384);
+	ApplePhone iphone;
+	iphone.setMfgID(1);
+	iphone.setMfgDate("15/11/2022");
+	iphone.setMfgName("Apple");
+	iphone.setiOSVersion("iOS 14");
+	iphone.turnOn();
+	iphone.makeVoiceCall(03224243115);
+	iphone.launchSiri();
+	iphone.launchPaper();
+	iphone.recordAudio();
+	iphone.turnOff();
+	iphone.sendSMS("Hello, how are you?", 03004567);
+	iphone.recordAudio();
+	iphone.turnOn();
+	iphone.makeVoiceCall(0324534554);
 
-	}
-	cout << endl;
-	cout << endl;
+	cout << endl << endl;
 
-	{
-		AndroidPhone samsung;
-		samsung.setAndroidPhone(2, "11/3/2022", "Samsung");
-		samsung.setiOSVersion("Android 10");
-		samsung.setKernalVersion("44");
-		samsung.turnOn();
-		samsung.turnOff();
-		samsung.launchSubwaySurfers();
-		samsung.launchFlynx();
-		samsung.turnOn();
-		samsung.makeVoiceCall(384);
-		samsung.recordAudio();
-	}
+	AndroidPhone oppo;
+	oppo.setAndroidPhone(2, "11/11/2022", "OPPO");
+	oppo.setiOSVersion("Android lollipop 11");
+	oppo.setKernalVersion("44");
+	oppo.turnOn();
+	oppo.launchSubwaySurfers();
+	oppo.turnOff();
+	oppo.launchSubwaySurfers();
+	oppo.launchFlynx();
+	oppo.turnOn();
+	oppo.makeVoiceCall(384);
+	oppo.recordAudio();
 
-	cout << endl;
-	cout << endl;
+	cout << endl << endl;
 
-	WindowsPhone wp;
-	wp.setWindowsPhone(3, "11/3/2022", "Microsoft");
-	wp.setProductVersion("Windows Phone");
-	wp.setBuildNumber("105860");
-	wp.turnOn();
-	wp.launchXboxGame();
-	wp.sendSMS("Message not allowed in lab", 02323);
-	wp.turnOff();
-	wp.launchFoundBite();
-	wp.turnOn();
-
+	WindowsPhone lumia;
+	lumia.setWindowsPhone(3, "15/10/2022", "Microsoft");
+	lumia.setProductVersion("Windows Phone 10");
+	lumia.setBuildNumber("734234");
+	lumia.turnOn();
+	lumia.launchXboxGame();
+	lumia.sendSMS("Message not allowed in lab", 02323);
+	lumia.turnOff();
+	lumia.launchFoundBite();
+	lumia.turnOn();
+	lumia.turnOff();
 
 }
