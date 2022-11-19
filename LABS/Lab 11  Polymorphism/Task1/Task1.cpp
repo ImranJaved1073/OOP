02c7c93dfbd92e41c19a83ebbbaf2f05127346aa
Imran Javed <96229355+ImranJaved1073@users.noreply.github.com>
2022-11-19 14:07:17 +0500

    Update Task1.cpp

--- LABS/Lab 11  Polymorphism/Task1/Task1.cpp
+++ LABS/Lab 11  Polymorphism/Task1/Task1.cpp
@@ -1,3 +1,4 @@
+//ship class
 #include <iostream>
 #include <string>
 #include "Ship.h"
@@ -9,7 +10,6 @@ using namespace std;
 
 int main()
 {
-	Ship* ships[4];
 	cout << "Which ship do you want to create?\n1.Cruise Ship\n2.Cargo Ship\n3.Battle Ship\n4.Simple Ship" << endl;
 	int choice;
 	cin >> choice;
@@ -20,67 +20,118 @@ int main()
 	}
 	if (choice == 1)
 	{
+		Ship* s;
 		string name;
 		string year;
 		int maxPassengers;
-		cout << "Enter the name of the ship: ";
-		cin >> name;
-		cout << "Enter the year the ship was built: ";
-		cin >> year;
-		cout << "Enter the maximum number of passengers: ";
-		cin >> maxPassengers;
-		ships[0] = new CruiseShip(maxPassengers, name, year);
-		ships[0]->print();
-		delete ships[0];
-		ships[0] = nullptr;
+		cout << "______________________CRUISE SHIP________________" << endl;
+		cout << "                ENTER CRUISE SHIP DATA " << endl;
+		cout << "_________________________________________________" << endl;
+		cout << "Enter the name of the Cruise Ship: ";
+		cin.ignore();
+		getline(cin, name);
+		cout << "Enter the year the Cruise Ship was made: ";
+		getline(cin, year);
+		cout << "Enter the maximum number of passengers the Cruise Ship can hold: ";
+		while (!(cin >> maxPassengers) || maxPassengers <= 0)
+		{
+			cout << "Invalid number of passengers. Please enter a number greater than 0: ";
+			cin.clear();
+			cin.ignore(1000, '\n');
+		}
+		s = new CruiseShip(maxPassengers, name, year);
+		cout << "_________________________________________________" << endl;
+		cout << "                CRUISE SHIP DATA " << endl;
+		cout << "_________________________________________________" << endl;
+		s->print();
+		delete s;
+		s = nullptr;
 	}
+
+
 	else if (choice == 2)
 	{
+		Ship* s;
 		string name;
 		string year;
 		int capacity;
-		cout << "Enter the name of the ship: ";
-		cin >> name;
-		cout << "Enter the year the ship was built: ";
-		cin >> year;
-		cout << "Enter the capacity of the ship: ";
-		cin >> capacity;
-		ships[1] = new CargoShip(capacity, name, year);
-		ships[1]->print();
-		delete ships[1];
-		ships[1] = nullptr;
+		cout << "______________________CARGO SHIP_________________" << endl;
+		cout << "                ENTER CARGO SHIP DATA " << endl;
+		cout << "_________________________________________________" << endl;
+		cout << "Enter the name of the Cargo Ship: ";
+		cin.ignore();
+		getline(cin, name);
+		cout << "Enter the year the Cargo Ship was made: ";
+		getline(cin, year);
+		cout << "Enter the capacity of the Cargo Ship: ";
+		while (!(cin >> capacity) || capacity <= 0)
+		{
+			cout << "Invalid capacity. Please enter a number greater than 0: ";
+			cin.clear();
+			cin.ignore(1000, '\n');
+		}
+		s = new CargoShip(capacity, name, year);
+		cout << "________________________________________________" << endl;
+		cout << "                CARGO SHIP DATA " << endl;
+		cout << "_________________________________________________" << endl;
+		s->print();
+		delete s;
+		s = nullptr;
+
 	}
 
 	else if (choice == 3)
 	{
+		Ship* s;
 		string name;
 		string year;
 		int totalNumOfMissiles;
-		cout << "Enter the name of the ship: ";
-		cin >> name;
-		cout << "Enter the year the ship was built: ";
-		cin >> year;
-		cout << "Enter the total number of missiles: ";
-		cin >> totalNumOfMissiles;
-		ships[2] = new BattleShip(totalNumOfMissiles, name, year);
-		ships[2]->print();
-		delete ships[2];
-		ships[2] = nullptr;
+		cout << "______________________BATTLE SHIP_________________" << endl;
+		cout << "                ENTER BATTLE SHIP DATA " << endl;
+		cout << "__________________________________________________" << endl;
+		cout << "Enter the name of the Battle Ship: ";
+		cin.ignore();
+		getline(cin, name);
+		cout << "Enter the year the Battle Ship was made: ";
+		getline(cin, year);
+		cout << "Enter the total number of missiles the Battle Ship can hold: ";
+		while (!(cin >> totalNumOfMissiles) || totalNumOfMissiles <= 0)
+		{
+			cout << "Invalid number of missiles. Please enter a number greater than 0: ";
+			cin.clear();
+			cin.ignore(1000, '\n');
+		}
+		s = new BattleShip(totalNumOfMissiles, name, year);
+		cout << "__________________________________________________" << endl;
+		cout << "                BATTLE SHIP DATA " << endl;
+		cout << "__________________________________________________" << endl;
+		s->print();
+		delete s;
+		s = nullptr;
 	}
 
 	else if (choice == 4)
 	{
+		Ship* s;
+		s = new Ship;
 		string name;
 		string year;
+		cout << "______________________SIMPLE SHIP_________________" << endl;
+		cout << "                ENTER SIMPLE SHIP DATA " << endl;
+		cout << "__________________________________________________" << endl;
 		cout << "Enter the name of the ship: ";
-		cin >> name;
+		cin.ignore();
+		getline(cin, name);
 		cout << "Enter the year the ship was built: ";
-		cin >> year;
-		ships[3] = new Ship(name, year);
-		ships[3]->print();
-		delete ships[3];
-		ships[3] = nullptr;
+		getline(cin, year);
+		s->setName(name);
+		s->setYear(year);
+		cout << "__________________________________________________" << endl;
+		cout << "                SIMPLE SHIP DATA " << endl;
+		cout << "__________________________________________________" << endl;
+		s->print();
+		delete s;
+		s = nullptr;
 	}
 
-	return 0;
 }
