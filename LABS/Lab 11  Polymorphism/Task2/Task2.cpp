214c196cbbd62cb6a8793034fc43646c26cfcf1b
Imran Javed <96229355+ImranJaved1073@users.noreply.github.com>
2022-11-19 12:37:29 +0500

    Update Task2.cpp

--- LABS/Lab 11  Polymorphism/Task2/Task2.cpp
+++ LABS/Lab 11  Polymorphism/Task2/Task2.cpp
@@ -7,14 +7,12 @@ using namespace std;
 
 int main()
 {
-	const int SIZE = 2;
-	Person* person[SIZE];
 	cout << "Which functionality you want to use?" << endl;
 	cout << "1. Student" << endl;
 	cout << "2. Teacher " << endl;
 	int choice;
 	cin >> choice;
-	while (choice < 1 || choice > SIZE)
+	while (choice < 1 || choice > 2)
 	{
 		cout << "Invalid choice. Please enter a number 1 or 2." << endl;
 		cin >> choice;
@@ -22,46 +20,94 @@ int main()
 	if (choice == 1)
 	{
 		string name; int age, numOfSubjects;
-		cout << "Enter name: ";
-		cin >> name;
-		cout << "Enter age: ";
-		cin >> age;
+		Person* p;
+		p = new Student;
+		int* marks;
+
+		cout << "Enter Student Data: " << endl;
+		cout << "Enter the name of the Student:";
+		cin.ignore();
+		getline(cin, name);
+		cout << "Enter the Age of the Student:";
+		while (!(cin >> age) || age < 0 || age > 100)
+		{
+			cout << "Invalid age. Please enter a number between 0 and 100." << endl;
+			cin.clear();
+			cin.ignore(1000, '\n');
+		}
+		p->setName(name);
+		p->setAge(age);
 		cout << "Enter number of subjects: ";
-		cin >> numOfSubjects;
-		int* marks = new int[numOfSubjects];
+		while (!(cin >> numOfSubjects) || numOfSubjects < 0)
+		{
+			cout << "Invalid number of subjects. Please enter a number greater than 0." << endl;
+			cin.clear();
+			cin.ignore(1000, '\n');
+		}
+		while (numOfSubjects < 0)
+		{
+			cout << "Enter Valid COurses..!" << endl;
+			cin >> numOfSubjects;
+		}
+		marks = new int[numOfSubjects];
 		cout << "Enter subjects:\n";
 		for (int i = 0; i < numOfSubjects; i++)
 		{
 			cout << "Enter marks for subject " << i + 1 << ": ";
-			cin >> marks[i];
+			while (!(cin >> marks[i]) || marks[i] < 0 || marks[i] > 100)
+			{
+				cout << "Invalid marks. Please enter marks between 0 and 100." << endl;
+				cin.clear();
+				cin.ignore(1000, '\n');
+			}
 		}
-		person[0] = new Student(name, age, marks, numOfSubjects);
-		person[0]->printInfo();
-		delete person[0];
-		person[0] = nullptr;
+		p->setMarks(marks, numOfSubjects);
+		p->printInfo();
+		delete p;
+		p= nullptr;
+		//------------------------------------------------------------------
 	}
 	else if (choice == 2)
 	{
-		string name;
-		int age;
+		string name; 
+		int age, numOfLectures;
+		Person* p;
+		p = new Teacher;
+		string* lecturesName;
+		
+		cout << "Enter Teacher Data: " << endl;
 		cout << "Enter name: ";
-		cin >> name;
-		cout << "Enter age: ";
-		cin >> age;
-		int numClasses;
-		cout << "Enter number of classes: ";
-		cin >> numClasses;
-		string* classes = new string[numClasses];
-		cout << "Enter classes:\n";
-		for (int i = 0; i < numClasses; i++)
+		cin.ignore();
+		getline(cin, name);
+		cout << "Enter Age of Teacher: ";
+		while(!(cin>>age) || age<0 || age>120)
 		{
-			cout << "Title of Lecture # " << i + 1 << ": ";
-			cin >> classes[i];
+			cout << "Enter valid Age" << endl;
+			cin.clear();
+			cin.ignore(1000, '\n');
 		}
-		person[1] = new Teacher(name, age, classes, numClasses);
-		person[1]->printInfo();
-		delete person[1];
-		person[1] = nullptr;
+		p->setName(name);
+		p->setAge(age);
+		cout << "Enter number of lectures: ";
+		while (!(cin >> numOfLectures) || numOfLectures < 0)
+		{
+			cout << "Enter Valid Lectures..!" << endl;
+			cin.clear();
+			cin.ignore(1000, '\n');
+		}
+		lecturesName = new string[numOfLectures];
+		cout << "Enter lectures:\n";
+		cin.ignore();
+		for (int i = 0; i < numOfLectures; i++)
+		{
+			cout << "Enter name of lecture " << i + 1 << ": ";
+			getline(cin, lecturesName[i]);
+		}
+		p->setLectures(lecturesName, numOfLectures);
+		p->printInfo();
+		delete p;
+		p = nullptr;
 	}
+
 	return 0;
 }
